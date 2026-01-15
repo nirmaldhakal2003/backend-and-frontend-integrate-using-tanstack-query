668f337f9bf1e95bdc64c0b2cc91face37aa5fc2
nirmaldhakal2003 <nirmalbsccsit2@gmail.com>
2026-01-15 21:54:20 +0545

    fixed set up issues in backend

--- src/controllers/user/createUser-controller.ts
+++
@@ -1,7 +0,0 @@
-import { Request, Response } from "express";
-export const createUserController = async (req: Request, res: Response) => {
-  const userData = req.body;
-  res
-    .status(201)
-    .send({ message: "User created successfully", data: userData });
-};
--- src/routers/user-routes.ts
+++
@@ -1,7 +0,0 @@
-import { Application } from "express";
-import { createUserController } from "../controllers/user/createUser-controller";
-
-export async function userRouter(app: Application) {
-  app.post("/users", createUserController);
-  
-}
